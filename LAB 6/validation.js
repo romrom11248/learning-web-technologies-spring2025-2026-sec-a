f7c08b7414e27cda1b264424ce9d1507e941efe7
romrom11248 <romrom11248@gmail.com>
2026-03-16 00:35:19 +0600

    Added dynamic validation in the form

--- LAB 6/validation.js
+++ LAB 6/validation.js
@@ -1,21 +1,23 @@
 const form=document.getElementById("profileForm");
+const nameInput = document.getElementById("fullName");
+const mailInput = document.getElementById("email");
+const genderRadios = document.getElementsByName("gender");
+
+const DAY = document.getElementById("dob_day");
+const MONTH = document.getElementById("dob_month");
+const YEAR = document.getElementById("dob_year");
+
+const bloodSelect = document.getElementById("bloodGroup");
+const degreeCheckboxes = document.getElementsByName("degree");
+const USERID = document.getElementById("userId");
+const fileInput = document.getElementById("profilePic");
+
+let ok = true; 
 
 
-form.addEventListener("submit", function(event) {
-   
-    document.getElementById("nameError").textContent = "";
-    document.getElementById("emailError").textContent = "";
-    document.getElementById("genderError").textContent = "";
-    document.getElementById("dobError").textContent = "";
-    document.getElementById("bloodError").textContent = "";
-    document.getElementById("degreeError").textContent = "";
-    document.getElementById("userIdError").textContent = "";
-    document.getElementById("photoError").textContent = "";
-
-    let ok = true;  
-
-    // Name validation
-    const nameInput = document.getElementById("fullName");
+// Name validation
+function Name(){ 
+    
     const namee = nameInput.value.trim();
     if (namee.length === 0) {
         document.getElementById("nameError").textContent = "Name is required";
@@ -44,9 +46,10 @@ form.addEventListener("submit", function(event) {
             }
         }
     }
-
-    // Email validation
-    const mailInput = document.getElementById("email");
+}
+// Email validation
+function email(){
+   
     const mail = mailInput.value.trim();
     if (mail.length === 0) {
         document.getElementById("emailError").textContent = "Email is required";
@@ -59,18 +62,23 @@ form.addEventListener("submit", function(event) {
             ok = false;
         }
     }
+}
 
-    // Gender validation 
-    const genderRadios = document.getElementsByName("gender");
+ // Gender validation 
+function gender(){
+   
     if (!(genderRadios[0].checked) && !(genderRadios[1].checked) && !(genderRadios[2].checked)) {
         document.getElementById("genderError").textContent = "Must select a gender";
         ok = false;
     }
+}
 
-    // Date of Birth validation
-    const day = document.getElementById("dob_day").value.trim();
-    const month = document.getElementById("dob_month").value.trim();
-    const year = document.getElementById("dob_year").value.trim();
+
+// Date of Birth validation
+function dob(){
+    const day = DAY.value.trim();
+    const month = MONTH.value.trim();
+    const year = YEAR.value.trim();
     if (day === "" || month === "" || year === "") {
         document.getElementById("dobError").textContent = "Date of birth cannot be empty";
         ok = false;
@@ -86,48 +94,83 @@ form.addEventListener("submit", function(event) {
             ok = false;
         }
     }
-
-    //  Blood Group validation 
-    const bloodSelect = document.getElementById("bloodGroup");
+}
+   
+    //  Blood Group validation
+function blood(){
+  
+   
     if (bloodSelect.value === "") {
         document.getElementById("bloodError").textContent = "Must select blood group";
         ok = false;
     }
+}
 
-    //  Degree validation 
-    const degreeCheckboxes = document.getElementsByName("degree");
-    let isChecked = false;
-    for (let i = 0; i < degreeCheckboxes.length; i++) {
-        if (degreeCheckboxes[i].checked) {
-            isChecked = true;
-            break;
-        }
-    }
-    if (!isChecked) {
-        document.getElementById("degreeError").textContent = "Must select at least one degree";
-        ok = false;
-    }
+//  Degree validation 
+function degree(){
+      
+     
+      let isChecked = false;
+      for (let i = 0; i < degreeCheckboxes.length; i++) {
+          if (degreeCheckboxes[i].checked) {
+              isChecked = true;
+              break;
+          }
+      }
+      if (!isChecked) {
+          document.getElementById("degreeError").textContent = "Must select at least one degree";
+          ok = false;
+      }
+}
 
-    // User ID validation 
-    const userId = document.getElementById("userId").value.trim();
+
+ // User ID validation 
+function id(){
+   const userId = USERID.value.trim();
     if (userId === "") {
         document.getElementById("userIdError").textContent = "User ID cannot be empty";
         ok = false;
     } else {
         const num = Number(userId);
-        if (isNaN(num) || num <= 0) {
+        if (isNaN(num) || num <= 0|| !Number.isInteger(num)) {
             document.getElementById("userIdError").textContent = "User ID must be a positive number";
             ok = false;
         }
     }
+}
 
-    // Photo validation 
-    const fileInput = document.getElementById("profilePic");
+  // Photo validation
+function image(){
+   
+   
     if (fileInput.files.length === 0) {
         document.getElementById("photoError").textContent = "Picture cannot be empty";
         ok = false;
     }
 
+}
+
+form.addEventListener("submit", function(event) {
+   
+    document.getElementById("nameError").textContent = "";
+    document.getElementById("emailError").textContent = "";
+    document.getElementById("genderError").textContent = "";
+    document.getElementById("dobError").textContent = "";
+    document.getElementById("bloodError").textContent = "";
+    document.getElementById("degreeError").textContent = "";
+    document.getElementById("userIdError").textContent = "";
+    document.getElementById("photoError").textContent = "";
+
+    ok=true;
+
+    Name();
+    email();
+    gender();
+    dob();
+    blood();
+    degree();
+    id();
+    image();
  
     if (!ok) {
         event.preventDefault();   
@@ -135,3 +178,15 @@ form.addEventListener("submit", function(event) {
         alert("Registration Successful");
     }
 });
+
+nameInput.addEventListener("input", function() {
+    Name();
+});
+mailInput.addEventListener("input", function() {
+    email();
+});
+
+USERID.addEventListener("input", function() {
+    id();
+});
+
