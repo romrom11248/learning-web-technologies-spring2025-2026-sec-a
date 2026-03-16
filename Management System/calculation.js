4d5ecd3ec7261bd78fcbd113903e4e1e0dbf10f6
romrom11248 <romrom11248@gmail.com>
2026-03-16 14:32:18 +0600

    Management Page

---
+++ Management System/calculation.js
@@ -0,0 +1,28 @@
+const quantity=document.getElementById("quantity");
+
+const display=document.getElementById("display");
+
+const unitPrice=1000;
+
+function calculate(){
+    let Quantity=quantity.valueAsNumber;
+
+    if(isNaN(Quantity)){
+        display.value="Quantity has to be a number";
+        Quantity=0;
+
+    }if(Quantity<0){
+        display.value="Error occurred";
+        Quantity=0;
+    }
+        let x=Quantity*unitPrice;
+        display.value="Total price "+ x+" tk";
+        
+        if(x>1000){
+            alert("Customer is eligible for gift coupon!");
+        }
+    
+
+}
+
+quantity.addEventListener("input",calculate);
